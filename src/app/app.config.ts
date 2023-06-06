80b3111abd56e5d545f0162b6827baf2d6d8e86a
Rohita Rani <rrani@ritehite.com>
2023-06-06 21:13:32 +0530

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 
 export const config = {
   oidc: {
-    clientId: '0oa1qzc5zusOO1KaH0h8',
-    issuer: 'https://ritehite.cloud/oauth2/aus1r1uyp4s4PxeIx0h8',
+    clientId: 'yqgwhjqwuy76873',
+    issuer: 'https://me/9897hgxjas',
     redirectUri: 'http://localhost:4200',
     scopes: ['openid', 'profile', 'email'],
   }
